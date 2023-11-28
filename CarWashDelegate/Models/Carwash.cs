3b410eb1cd78d2242c03b2f92cc1a4c0e8ece949
Samuel Legrain <samuel.legrain@bstorm.be>
2023-11-28 17:03:38 +0100

    Add Correction CarWash

---
+++ CarWashDelegate/Models/Carwash.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarWashDelegate.Models
+{
+    public delegate void CarWashAction(Voiture voiture);
+    public class Carwash
+    {
+        private CarWashAction _traitement;
+
+        public Carwash()
+        {
+            /*
+            _traitement = Preparer;
+            _traitement += Laver;
+            _traitement += Secher;
+            _traitement += Finaliser;
+            */
+
+            _traitement = delegate (Voiture voiture) { Console.WriteLine($"Je prépare la voiture : {voiture.Plaque}"); };
+            _traitement += delegate (Voiture voiture) { Console.WriteLine($"Je lave la voiture : {voiture.Plaque}"); };
+            _traitement += delegate (Voiture voiture) { Console.WriteLine($"Je sèche la voiture : {voiture.Plaque}"); };
+            _traitement += delegate (Voiture voiture) { Console.WriteLine($"Je finalise la voiture : {voiture.Plaque}"); };
+        }
+        public void Traiter(Voiture voiture)
+        {
+            _traitement(voiture);
+        }
+
+        /*
+        private void Preparer(Voiture voiture)
+        {
+            Console.WriteLine($"Je prépare la voiture : {voiture.Plaque}");
+        }
+
+        private void Laver(Voiture voiture)
+        {
+            Console.WriteLine($"Je lave la voiture : {voiture.Plaque}");
+        }
+        private void Secher(Voiture voiture)
+        {
+            Console.WriteLine($"Je sèche la voiture : {voiture.Plaque}");
+        }
+        private void Finaliser(Voiture voiture)
+        {
+            Console.WriteLine($"Je finalise la voiture : {voiture.Plaque}");
+        }*/
+    }
+}
---
+++ CarWashDelegate/Program.cs
@@ -0,0 +1,20 @@
+﻿using CarWashDelegate.Models;
+
+namespace CarWashDelegate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Voiture v1 = new Voiture("1-ABC-123");
+            Voiture v2 = new Voiture("2-ABC-123");
+            Voiture v3 = new Voiture("D-ABC-123");
+
+            Carwash carwash = new Carwash();
+
+            carwash.Traiter(v1);
+            carwash.Traiter(v2);
+            carwash.Traiter(v3);
+        }
+    }
+}
